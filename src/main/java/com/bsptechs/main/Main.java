7d0cff66e0989ea8e8f7ccb3a8daceef22198697
Sarkhan Rasullu <sarkhanrasullu@gmail.com>
2018-11-17 22:20:18 +0400

    removed unused dependencies

--- src/main/java/com/bsptechs/main/Main.java
+++ src/main/java/com/bsptechs/main/Main.java
@@ -21,10 +21,12 @@ public class Main extends javax.swing.JFrame {
  
     public Main() {
         initComponents();
+        System.out.println("Salam");
         btnNewQuery.setEnabled(false);
         menuNewQuery.setEnabled(false);
         this.setExtendedState(JFrame.MAXIMIZED_BOTH);
         setIcons();
+        System.out.println("SalamSalam test222");
     }
     
     public void setIcons(){
